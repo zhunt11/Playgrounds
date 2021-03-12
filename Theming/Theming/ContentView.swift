644b49c10ab7039a2af6b3216367f4040c291b6c
Zachary Hunt <zhunt11@icloud.com>
2021-03-12 11:02:01 -0700

    Adding new project to test an idea for component themeing

---
+++ Theming/Theming/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Theming
+//
+//  Created by Zachary Hunt on 3/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Theming/Theming/ThemingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemingApp.swift
+//  Theming
+//
+//  Created by Zachary Hunt on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ThemingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
